e0ae2fef09e1bce9a28112c5337bcf396b9c8eed
tkuriyama <taro.kuriyama@gmail.com>
2020-08-03 15:55:52 -0400

    Initializing repo.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# hsom
-Haskell School of Music
+## Haskell School of Music
+
+Collection of exercises from reading [The Haskelll School of Music](https://www.amazon.com/Haskell-School-Music-Signals-Symphonies/dp/1108416756/).
+
---
+++ exercises/chapter2/ex2_1.hs
@@ -0,0 +1,29 @@
+import Euterpea
+
+type Major = Bool
+
+-- 2.2
+
+t251 :: Music Pitch
+t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+           gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
+           cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+           in dMinor :+: gMajor :+: cMajor
+
+triad :: Music Pitch -> Major -> Music Pitch
+triad n m = n :=: n' :=: n''
+  where n' = transpose (if m then 4 else 3) n
+        n'' = transpose 7 n
+
+twoFiveOne :: Pitch -> Dur -> Music Pitch
+twoFiveOne p d = let n = note d p
+                     five = triad (transpose 7 n) True
+                     two = triad (transpose 2 n) False
+                     one = triad (note (d*2) p) True
+                 in two :+: five :+: one
+
+-- 2.2
+
+data BluesPitchClass = Ro MT Fo Fi MS
+
+type BluesPitch = (BluesPitchClass, Octave)
